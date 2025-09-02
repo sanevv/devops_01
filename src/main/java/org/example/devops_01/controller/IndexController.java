e142d318f0ee8276bb42f74eadee6e90e64bcccd
sanhae.kang <sanevv@naver.com>
2025-09-02 09:41:02 +0900

    first commit

---
+++ src/main/java/org/example/devops_01/controller/IndexController.java
@@ -0,0 +1,12 @@
+package org.example.devops_01.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
